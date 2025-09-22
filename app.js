2b80b6cdd64abd78533480bc7ebe8d9d334b4900
G. <rmgolamrabbani@gmail.com>
2025-09-22 19:12:44 +0600

    sj variable

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Hello, World!");
+console.log("This is a sample JavaScript file.");
+
+let a = 5;
+let b = 10;
+let sum = a + b;
+
+console.log (sum);
+
+console.log(a*b);
+alert("Hello!");
